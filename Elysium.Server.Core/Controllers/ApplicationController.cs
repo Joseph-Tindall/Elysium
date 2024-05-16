650f5a3c92f51e443bcfbf1e6b1f24aa576b9b8b
Joseph Tindall <77989761+Joseph-Tindall@users.noreply.github.com>
2024-05-16 15:21:20 -0400

    Core backup for MVC refresh

--- Elysium.Server.Core/Controllers/ApplicationController.cs
+++ Elysium.Server.Core/Controllers/ApplicationController.cs
@@ -8,150 +8,149 @@ using Microsoft.EntityFrameworkCore;
 using Elysium.Server.Core.Data;
 using Elysium.Server.Core.Models;
 
-namespace Elysium.Server.Core.Controllers
+namespace Elysium.Server.Core.Controllers;
+
+public class ApplicationController : Controller
 {
-    public class ApplicationController : Controller
+    private readonly ApplicationContext _context;
+
+    public ApplicationController(ApplicationContext context)
     {
-        private readonly ApplicationContext _context;
+        _context = context;
+    }
 
-        public ApplicationController(ApplicationContext context)
+    // GET: Application
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Application.ToListAsync());
+    }
+
+    // GET: Application/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        // GET: Application
-        public async Task<IActionResult> Index()
+        var application = await _context.Application
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (application == null)
         {
-            return View(await _context.Application.ToListAsync());
+            return NotFound();
         }
 
-        // GET: Application/Details/5
-        public async Task<IActionResult> Details(int? id)
+        return View(application);
+    }
+
+    // GET: Application/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Application/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Name,Path,InstallCommand,Arguments")] Application application)
+    {
+        if (ModelState.IsValid)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var application = await _context.Application
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (application == null)
-            {
-                return NotFound();
-            }
-
-            return View(application);
-        }
-
-        // GET: Application/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Application/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Path,InstallCommand,Arguments")] Application application)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(application);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(application);
-        }
-
-        // GET: Application/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var application = await _context.Application.FindAsync(id);
-            if (application == null)
-            {
-                return NotFound();
-            }
-            return View(application);
-        }
-
-        // POST: Application/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Path,InstallCommand,Arguments")] Application application)
-        {
-            if (id != application.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(application);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ApplicationExists(application.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(application);
-        }
-
-        // GET: Application/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var application = await _context.Application
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (application == null)
-            {
-                return NotFound();
-            }
-
-            return View(application);
-        }
-
-        // POST: Application/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var application = await _context.Application.FindAsync(id);
-            if (application != null)
-            {
-                _context.Application.Remove(application);
-            }
-
+            _context.Add(application);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        return View(application);
+    }
 
-        private bool ApplicationExists(int id)
+    // GET: Application/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
         {
-            return _context.Application.Any(e => e.Id == id);
+            return NotFound();
         }
+
+        var application = await _context.Application.FindAsync(id);
+        if (application == null)
+        {
+            return NotFound();
+        }
+        return View(application);
+    }
+
+    // POST: Application/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Path,InstallCommand,Arguments")] Application application)
+    {
+        if (id != application.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(application);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ApplicationExists(application.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(application);
+    }
+
+    // GET: Application/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var application = await _context.Application
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (application == null)
+        {
+            return NotFound();
+        }
+
+        return View(application);
+    }
+
+    // POST: Application/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var application = await _context.Application.FindAsync(id);
+        if (application != null)
+        {
+            _context.Application.Remove(application);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool ApplicationExists(int id)
+    {
+        return _context.Application.Any(e => e.Id == id);
     }
 }
--- Elysium.Server.Core/Controllers/Applications/Install.cs
+++ Elysium.Server.Core/Controllers/Applications/Install.cs
@@ -1,15 +1,15 @@
+using Elysium.Server.Core.Data;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Elysium.Server.Core.Controllers.Applications
+namespace Elysium.Server.Core.Controllers.Applications;
+
+[Route("api/v1/applications/[controller]")]
+[ApiController]
+public class Install(ILogger<Install> logger, ApplicationContext context) : ControllerBase
 {
-    [Route("api/v1/applications/[controller]")]
-    [ApiController]
-    public class Install(ILogger<Install> logger) : ControllerBase
+    [HttpPost]
+    public void OnPost()
     {
-        [HttpPost]
-        public void OnPost()
-        {
-            logger.LogInformation("World");
-        }
+        logger.LogInformation("World");
     }
 }
