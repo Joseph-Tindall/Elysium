c291c19f25a42c023d0c730f0b9c5953ec2ffe8c
Joseph Tindall <77989761+Joseph-Tindall@users.noreply.github.com>
2024-05-20 10:32:11 -0400

    Added Post with Body support

---
+++ Elysium.Server.Core/Controllers/Applications/Install.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Elysium.Server.Core.Models;
+
+namespace Elysium.Server.Core.Controllers.Applications;
+
+[Route("api/v1/applications/[controller]")]
+[ApiController]
+public class Install(ILogger<Install> logger) : Controller
+{
+    [HttpPost]
+    public IActionResult ReadFromBody([FromBody] Application model)
+    {
+        logger.LogInformation("Hello world!");
+        var message = $"Application Data => Path: {model.Path}, Arguments: {model.Arguments}";
+        return Ok(message);
+    }
+}
