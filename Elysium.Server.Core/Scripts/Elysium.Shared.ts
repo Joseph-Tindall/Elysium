dfd86bd03fe92e8296b3524479e8edebf89faf58
Joseph Tindall <77989761+Joseph-Tindall@users.noreply.github.com>
2024-06-11 12:57:16 -0400

    Updated toggleFullListSelection (#1)

--- Elysium.Server.Core/Scripts/Elysium.Shared.ts
+++ Elysium.Server.Core/Scripts/Elysium.Shared.ts
@@ -1,25 +1,25 @@
-﻿document.addEventListener('DOMContentLoaded', () => {
-    const toggleCheckboxes = document.querySelectorAll('.toggleCheckbox');
-    toggleCheckboxes.forEach((checkbox) => {
+﻿document.addEventListener('DOMContentLoaded', (): void => {
+    const toggleCheckboxes: NodeListOf<Element> = document.querySelectorAll('.toggleCheckbox');
+    toggleCheckboxes.forEach((checkbox: Element) => {
         checkbox.addEventListener('change', toggleFullListSelection);
     });
 });
 
 function toggleFullListSelection(event: Event): void {
-    const target = event.target as HTMLElement;
-    const table = target.closest('table');
+    const target: HTMLElement = event.target as HTMLElement;
+    const table: HTMLTableElement = target.closest('table');
     if (!table) return;
     
-    const checkboxes = table.querySelectorAll('input[type="checkbox"]');
+    const checkboxes: NodeListOf<Element> = table.querySelectorAll('input[type="checkbox"]');
 
-    let allChecked = true;
-    checkboxes.forEach((checkbox) => {
+    let allChecked: boolean = true;
+    checkboxes.forEach((checkbox: Element): void => {
         if (checkbox === target) return;
         if (!(checkbox instanceof HTMLInputElement)) return;
         if (!checkbox.checked) allChecked = false;
     });
     
-    checkboxes.forEach((checkbox) => {
+    checkboxes.forEach((checkbox: Element): void => {
         if (!(checkbox instanceof HTMLInputElement)) return;
         checkbox.checked =!allChecked;
     });
